bb79b7143cfcc3529d720a38af2b6f0053eb6858
vjbravo123 <128238350+vjbravo123@users.noreply.github.com>
2023-04-16 15:12:31 +0530

    changed for cors

--- src/server.js
+++ src/server.js
@@ -4,10 +4,21 @@ const routertesting = require("./router")
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+
+const PORT = process.env.PORT || 5000;
+
+//cross-orign request handler
+const corsOptions = {
+  origin:'*',
+  methods:['GET','POST'],
+  allowedHeaders:['content-Type','Authorization']
+};
+
 //Middlewares:-----
 
 //CORS-CROSS ORIGIN RESOURCE SHARING FOR USING BY ANOTHER DEVICES OR ORIGIN
-app.use(cors());
+app.use(cors(corsOptions));
+// app.use(cors());
 //TO PARSE THE REQUEST BODY 
 app.use(bodyParser.json());
 
@@ -16,6 +27,6 @@ app.use(bodyParser.json());
 app.use('/',routertesting);
 
 
-app.listen(5000,()=>{
+app.listen(PORT,()=>{
     console.log("server running on port 5000");
 })
